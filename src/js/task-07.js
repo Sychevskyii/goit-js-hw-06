a7fb003527149873e00563011f6ef3c40bd646ed
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-05 11:11:50 +0200

    update task 7

---
+++ src/js/task-07.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+const minValue = parseInt(inputEl.getAttribute('min'));
+const maxValue = parseInt(inputEl.getAttribute('max'));
+
+inputEl.addEventListener('change', () => {
+    textEl.insertAdjacentHTML
+})
