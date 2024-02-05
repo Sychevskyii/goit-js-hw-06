4d81b3379ad22d0742449d86d20e1714a1b19b1f
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-05 13:32:29 +0200

    update task 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,10 +1,10 @@
-const inputEl = document.querySelector('#font-size-control');
-const textEl = document.querySelector('#text');
+const inputEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+const defaultValue = parseInt(inputEl.value);
+textEl.setAttribute("style", `font-size:${defaultValue}px`);
 
+inputEl.addEventListener("input", () => {
+  let inputElvalue = parseInt(inputEl.value);
 
-
-inputEl.addEventListener('input', () => {
-    let inputElvalue = parseInt(inputEl.value);
-    
-    textEl.setAttribute('style', `font-size:${inputElvalue}px`);
-})
+  textEl.setAttribute("style", `font-size:${inputElvalue}px`);
+});
