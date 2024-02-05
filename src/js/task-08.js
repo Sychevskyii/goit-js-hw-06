b4c9a58452d32e3fd440f492b47dff943d25d28f
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-05 15:44:16 +0200

    update task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: {email, password}
+    } = event.currentTarget;
+
+    if (email.value && password.value !== '') {
+        const dataForm = {};
+        const elements = formEl.elements;
+        for (let i = 0; i < elements.length; i += 1) {
+            let element = elements[i];
+
+            if (element.type !== 'submit') {
+                dataForm[element.name] = element.value;
+            }
+        }
+        console.log(dataForm);
+        // dataForm[email.name, password.name] = email.value, password.value;
+        // console.log(dataForm);
+    } else {
+        alert('Any field cannot be empty');
+    }
+})
